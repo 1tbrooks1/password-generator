f9ef7da1b951fc2b27edb897be58dbaded6078ea
tbrooks <tbrooks321@yahoo.com>
2021-12-10 01:54:26 -0500

    created multiple 1f statements to acquire criteria for password

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,20 +1,29 @@
 // Assignment code here
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 // Arrays for possible characters for password
-lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-characters = ["`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ":", ";", "'", "<", ">", "?", "/"];
+var passwordLength;
+var chooseUpper;
+var chooseLower;
+var chooseNumbers;
+var chooseSpecial;
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ":", ";", "'", "<", ">", "?", "/"];
 
 
 // Start of the function to being generating password
 function generatePassword () {
+  var passwordHolder = [];
   // Asks user for character amount 
-    passwordLength = prompt("How many characters would you like to have in your password? Please choose a number between 8 and 128.");
+    var passwordLength = parseInt(prompt("How many characters would you like to have in your password? Please choose a number between 8 and 128."));
     // Checks criteria for password length
-      if (passwordLength === "") {
+      if (passwordLength === "" || isNaN(passwordLength)) {
         alert("Please enter a value between 8 and 128.");
-        return;
+        generatePassword();
     } else if (passwordLength < 8 || passwordLength > 128) {
         alert("Please enter a number between 8 and 128");
  
@@ -23,24 +32,44 @@ function generatePassword () {
       chooseUpper = confirm("Do you want your password to contain upper case letters?");
       chooseLower = confirm("Do you want your password to contain lower case letters?");
       chooseNumbers = confirm("Do you want your password to contain numbers?");
-      chooseCharacters = confirm("Do you want your password to contain special characters?");
+      chooseSpecial = confirm("Do you want your password to contain special characters?");
     }
-  };
 
+  if (chooseUpper) {
+    var bigLetter = upper[Math.floor(Math.random() * upper.length)];
+    passwordHolder.push(bigLetter);
+  }
 
-  // Chosen password criteria from user
+  if (chooseLower) {
+    var smallLetter = lower[Math.floor(Math.random() * lower.length)];
+    passwordHolder.push(smallLetter);
+  }
 
+  if (chooseNumbers) {
+    var number = numbers[Math.floor(Math.random() * numbers.length)];
+    passwordHolder.push(number);
+  }
 
-  // for loop to create lengthe of password
- for (var i = 0; i < passwordLength; i++)
-    var passwordOptions = passwordCriteria[Math.floor(Math.random() * passwordCriteria.passwordLength)];
-    password.push(passwordOptions);
+  if (chooseSpecial) {
+    var different = special[Math.floor(Math.random() * special.length)];
+    passwordHolder.push(different);
+  }
 
+  if (!chooseUpper && !chooseLower && !chooseNumbers && !chooseSpecial) {
+    alert("You must choose at least one criteria to proceed.");
+  }
 
+var passwordArray = [];
 
+for (i = 0; i < passwordLength; i++) {
+  var pickPassword = passwordHolder[Math.floor(Math.random () * passwordHolder.length)];
+  passwordArray.push(pickPassword);
+}
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  return passwordArray.join("");
+
+}
+
 
 // Write password to the #password input
 function writePassword() { 
